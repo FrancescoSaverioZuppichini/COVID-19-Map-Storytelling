d662b66cc6ed5744b5ddad27422a5116190d2cd9
francesco <zuppif@usi.ch>
2020-03-11 16:47:18 +0100

    gitflow-feature-stash: docker

---
+++ server/public/precache-manifest.8da0591e088db62a93eb7eeb7db0f373.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef8dee14d2f4e3764c24454dedc3828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bd386cc4284264ab6b1",
+    "url": "/static/css/main.2577aa85.chunk.css"
+  },
+  {
+    "revision": "2d9f6f0f34070a539d9d",
+    "url": "/static/js/2.3ffd07ec.chunk.js"
+  },
+  {
+    "revision": "2bd386cc4284264ab6b1",
+    "url": "/static/js/main.f268361f.chunk.js"
+  },
+  {
+    "revision": "e30d5fefce4ab9a3876e",
+    "url": "/static/js/runtime~main.728696f1.js"
+  }
+]);
